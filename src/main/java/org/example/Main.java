125bed2264c1bca8b15fb00bb74c4a7f6b5af952
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-05-23 18:22:55 +0200

    Use methods with parameters or return values

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,22 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo Daniel");
+        congratulate("Daniel");
+        congratulate("Florian");
+        congratulate("Marcell");
+        String summary = composeSummary();
+        System.out.println(summary);
+    }
+
+    public static void congratulate(String name) {
+        System.out.println("Hallo " + name);
         System.out.println("Heute ist Dein Geburtstag!");
         for (int countD = 0; countD < 3; countD++) {
             System.out.println("Hurra!");
         }
+    }
 
-        System.out.println("Hallo Florian");
-        System.out.println("Heute ist Dein Geburtstag!");
-        for (int countF = 0; countF < 3; countF++) {
-            System.out.println("Hurra!");
-        }
-
-        System.out.println("Hallo Marcell");
-        System.out.println("Heute ist Dein Geburtstag!");
-        for (int countM = 0; countM < 3; countM++) {
-            System.out.println("Hurra!");
-        }
+    public static String composeSummary() {
+        return "Alles Gute euch 3en";
     }
 }
