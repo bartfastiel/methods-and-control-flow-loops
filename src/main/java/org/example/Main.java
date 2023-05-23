83b26262c67d589b9e9931d80e21acd0c734b558
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-05-23 17:39:15 +0200

    Implement example (sequentially)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hallo Daniel");
+        System.out.println("Heute ist Dein Geburtstag!");
+        System.out.println("Hurra!");
+        System.out.println("Hurra!");
+        System.out.println("Hurra!");
+
+        System.out.println("Hallo Florian");
+        System.out.println("Heute ist Dein Geburtstag!");
+        System.out.println("Hurra!");
+        System.out.println("Hurra!");
+        System.out.println("Hurra!");
+
+        System.out.println("Hallo Marcell");
+        System.out.println("Heute ist Dein Geburtstag!");
+        System.out.println("Hurra!");
+        System.out.println("Hurra!");
+        System.out.println("Hurra!");
+
+
     }
 }
